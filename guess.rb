994ea1d6e17c351bac2845938038ccbbb088731b
Fisher Adelakin <temidayoadelakin@gmail.com>
2013-10-19 21:08:27 -0400

    initial logic

---
+++ guess.rb
@@ -0,0 +1,14 @@
+print "Guess a number between 0 and 100\n"
+
+puts "Enter your guess: "
+guessNumber = gets.to_i
+
+number = rand(100)
+
+if guessNumber == number then
+	puts "You got it right!"
+else if guessNumber > number then
+	puts "Too high"
+else if guessNumber < number then
+	puts "Too low"
+end
