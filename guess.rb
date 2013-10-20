fac368bbd205679e08daa336319532ad744ec7f8
Fisher Adelakin <temidayoadelakin@gmail.com>
2013-10-19 21:17:19 -0400

    finished rewrite

--- guess.rb
+++ guess.rb
@@ -5,6 +5,24 @@ num_guesses = 0
 
 loop do 
 
-	
+	print "Please enter a guess from 1 to 100: "
+	guess = gets.chomp.to_i
+	num_guesses +=1
+
+	unless guess == number
+		message = if guess > number
+			"Too high"
+		else
+			"Too low"
+		end
+
+		puts message
+
+	else
+
+		puts "You got it!"
+		puts "It took you #{num_guesses} guesses."
+		exit
+	end
 
 end
